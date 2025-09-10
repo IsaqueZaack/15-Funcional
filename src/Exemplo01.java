cc07bb2dbea1cdfeb86a27ca96aed144fe17a5ce
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-10 11:33:54 -0300

    Exercicio 01

---
+++ src/Exemplo01.java
@@ -0,0 +1,22 @@
+import java.util.stream.IntStream;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        int[] x = {3, 4, 1, 7, 8};
+        IntStream fluxo = IntStream.of(x);
+
+        // filtragem dos números ímpares
+//        fluxo.filter(valor -> valor % 2 != 0)
+//                .forEach(System.out::println);
+
+        // filtragem dos números pares e após triplicar cada número
+//        fluxo.filter(valor -> valor % 2 == 0)
+//                .map(valor -> valor * 3)
+//                .forEach(System.out::println);
+
+        // soma dos elementos usando reduce()
+        int soma = fluxo.reduce(0, (total, i) -> total + i);
+        System.out.println(soma);
+
+    }
+}
---
+++ src/exercicio01/Exercicio01.java
@@ -0,0 +1,16 @@
+package exercicio01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        String[] aux = {"José", "Ana Paula", "Ana Carolina", "Vinicius", "Alberto"};
+        List<String> lista = Arrays.asList(aux);
+
+        lista.stream().filter(nome -> nome.startsWith("A"))
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+
+    }
+}
